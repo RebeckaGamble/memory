2b8756b7f7edf34a59d0973ebaa8e0a93993936c
Rebecka <rebeckagamble@hotmail.com>
2023-09-24 21:11:47 +0200

    data-framework on memory cards, functions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,57 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false; //so you can't flip to many cards at once
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+  if (lockBoard) return;
+    if (this === firstCard) return; //if double clicking on one card, no match!
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    //first time flipped
+    hasFlippedCard = true;
+    firstCard = this;
+    //if true, stop execution
+    return;
+  }
+  //second click
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+function checkForMatch() {
+  //do the cards match?
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  //match
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  //if no match, set timeout before flipping card back around
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 1500);
+}
+
+//after each round, reset board - destructuring assignment
+function resetBoard() {
+[hasFlippedCard, lockBoard] = [false, false];
+[firstCard, secondCard] = [null, null]
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
