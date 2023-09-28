4974401a5d1b8390dd111d23d581a7981461aff1
Rebecka <rebeckagamble@hotmail.com>
2023-09-29 00:03:40 +0200

    added confetti

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const initialTimeLimit = 60; //time limit for the game
   const cards = document.querySelectorAll(".memory-card");
   const startGameBtn = document.getElementById("startGameBtn");
   const tryAgainBtn = document.getElementById("tryAgainBtn");
@@ -6,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const playAgainBtn = document.getElementById("playAgainBtn");
   const gameOverMsg = document.getElementById("gameOverMsg");
   const timerDisplay = document.getElementById("timer");
+  const confettiContainer = document.querySelector(".confetti-container");
 
   let hasFlippedCard = false;
   let lockBoard = true; //so you can't flip cards
@@ -14,13 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const totalPairs = cards.length / 2;
   let gameStarted = false;
   let timerInterval;
-  let hasWon = false;
+  let confettiActive = false;
+  let isMatch = false;
+
 
   function flipCard() {
     if (
       lockBoard ||
       !gameStarted ||
-      timerDisplay.textContent === "Time left: 0"
+      timerDisplay.textContent === "Time left: 0" ||
+      this.classList.contains("flip")
     )
       return; //lock the board when game is not started/time left
     if (this === firstCard) return; //if double clicking on one card, no match!
@@ -33,20 +38,32 @@ document.addEventListener("DOMContentLoaded", function () {
       firstCard = this;
       //if true, stop execution
       return;
-    }
-    //second click
-    hasFlippedCard = false;
-    secondCard = this;
+    } else {
+ //second click
+ hasFlippedCard = false;
+ secondCard = this;
 
-    checkForMatch();
+ checkForMatch();
+    }
+   
+/*
+    if (isMatch) {
+      firstCard.classList.add("match");
+      secondCard.classList.add("match");
+    }*/
   }
 
   function checkForMatch() {
     //do the cards match?
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
+    if (isMatch) {
+      disableCards();
+    } else {
+      unflipCards();
+    }
   }
 
+  //
   function disableCards() {
     //match
     firstCard.removeEventListener("click", flipCard);
@@ -58,11 +75,12 @@ document.addEventListener("DOMContentLoaded", function () {
     resetBoard();
 
     if (matchedPairs === totalPairs) {
-        clearInterval(timerInterval);
-        gameOver(true);
+      clearInterval(timerInterval);
+      gameOver(true);
     }
   }
 
+  //no match, flip cards back around
   function unflipCards() {
     lockBoard = true;
     //if no match, set timeout before flipping card back around
@@ -86,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
     [firstCard, secondCard] = [null, null];
   }
 
-  //immediately invoked function expression
+  //immediately invoked function expression, shuffle cards
   (function shuffle() {
     cards.forEach((card) => {
       //shuffle the cards, give cards a random nr
@@ -97,7 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   //timer
   function timer() {
-    let sec = 60;
+    let sec = initialTimeLimit;
 
     timerInterval = setInterval(function () {
       timerDisplay.textContent = "Time left: " + sec;
@@ -111,7 +129,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1000);
   }
 
-  //start game
+  //start game first time
   function startGame() {
     if (!gameStarted) {
       //start the timer
@@ -133,16 +151,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  //when player wants to play again
   function playAgain() {
     if (gameStarted) {
-        hasWon= false;
-        matchedPairs = 0;
-      timerDisplay.textContent = "Time left: 60";
+      matchedPairs = 0;
+      //stop and reset timer
+      clearInterval(timerInterval);
+      timerDisplay.textContent = "Time left: " + initialTimeLimit;
+
+      removeConfetti();
 
       cards.forEach((card) => {
         card.classList.remove("flip");
         card.classList.remove("match");
       });
+      (function shuffle() {
+        cards.forEach((card) => {
+          let randomPos = Math.floor(Math.random() * 12);
+          card.style.order = randomPos;
+        });
+      })();
+
+      //start timer
+      timer();
 
       lockBoard = false;
       //reset board
@@ -150,20 +181,62 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  //get random color on the confetti
+  function getRandomColor() {
+    const letters = "01234567ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  //winner confetti
+  function createConfetti() {
+    for (let i = 0; i < 100; i++) {
+      const confetti = document.createElement("div");
+      confetti.className = "confetti";
+      confetti.style.left = `${Math.random() * 100}%`;
+      confetti.style.animationDuration = `${Math.random() * 6 + 2}s`;
+      confetti.style.animationDelay = `${Math.random() * 2}s`;
+
+      const randomColor = getRandomColor();
+      confetti.style.backgroundColor = randomColor;
+
+      confettiContainer.appendChild(confetti);
+    }
+  }
+
+  //to stop the confetti
+  function removeConfetti() {
+    while (confettiContainer.firstChild) {
+      confettiContainer.removeChild(confettiContainer.firstChild);
+    }
+  }
+
+  //if won game, show modal:
   function showWinModal() {
-    hasWon = true;
     clearInterval(timerInterval);
     gameOverMsg.textContent = "Congrats, you won!";
     gameOverModal.style.display = "block";
     tryAgainBtn.style.display = "none";
     playAgainBtn.style.display = "inline-block";
+    if (!confettiActive) {
+      createConfetti();
+      confettiActive = true;
+    }
   }
 
+  //if lost game, show:
   function showLoseModal() {
     gameOverMsg.textContent = "Sorry, time ran out. Try again!";
+    gameOverModal.classList.add("show");
     gameOverModal.style.display = "block";
     tryAgainBtn.style.display = "inline-block";
     playAgainBtn.style.display = "none";
+    gameOverModal.style.opacity = 1;
+
+    gameOverMsg.classList.add("loserGlow");
   }
 
   function gameOver(isWinner) {
@@ -174,19 +247,23 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  //event listeners
+  //event listeners:
+  //starting the game first time
   startGameBtn.addEventListener("click", startGame);
-  //if winner wants to play again
+
+  //if won and wants to play again
   playAgainBtn.addEventListener("click", () => {
     gameOverModal.style.display = "none";
     playAgain();
   });
-  //if looser wants to play again
+
+  //if lost and wants to play again
   tryAgainBtn.addEventListener("click", () => {
     gameOverModal.style.display = "none";
     playAgain();
   });
 
+  //flipping card
   cards.forEach((card) =>
     card.addEventListener("click", () => flipCard.call(card))
   );
